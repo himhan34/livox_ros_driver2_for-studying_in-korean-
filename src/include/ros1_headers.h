3f65aa0559728638d3f3f0a2b1af592017cfc152
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 10:00:33 +0900

    Update ros1_headers.h

--- src/include/ros1_headers.h
+++ src/include/ros1_headers.h
@@ -24,31 +24,32 @@
 
 // Denoting headers specifically used for building ROS1 Driver.
 
-#ifndef ROS1_HEADERS_H_
-#define ROS1_HEADERS_H_
+#ifndef ROS1_HEADERS_H_ // ROS1_HEADERS_H_가 정의되지 않았을 때
+#define ROS1_HEADERS_H_ // ROS1_HEADERS_H_를 정의
 
-#include <thread>
-#include <future>
+#include <thread> // 스레드 사용을 위한 헤더 파일 포함
+#include <future> // 비동기 작업을 위한 헤더 파일 포함
 
-#include <ros/ros.h>
-#include <rosbag/bag.h>
-#include <pcl_ros/point_cloud.h>
-#include <sensor_msgs/Imu.h>
-#include <sensor_msgs/PointCloud2.h>
-#include "livox_ros_driver2/CustomMsg.h"
-#include "livox_ros_driver2/CustomPoint.h"
+#include <ros/ros.h> // ROS 기본 헤더 파일 포함
+#include <rosbag/bag.h> // ROSbag 사용을 위한 헤더 파일 포함
+#include <pcl_ros/point_cloud.h> // PCL 포인트 클라우드 사용을 위한 헤더 파일 포함
+#include <sensor_msgs/Imu.h> // IMU 센서 메시지 사용을 위한 헤더 파일 포함
+#include <sensor_msgs/PointCloud2.h> // 포인트 클라우드2 메시지 사용을 위한 헤더 파일 포함
+#include "livox_ros_driver2/CustomMsg.h" // CustomMsg 메시지 사용을 위한 헤더 파일 포함
+#include "livox_ros_driver2/CustomPoint.h" // CustomPoint 메시지 사용을 위한 헤더 파일 포함
 
-
+// 디버그 매크로 정의
 #define DRIVER_DEBUG(node, ...) ROS_DEBUG(__VA_ARGS__)
 #define DRIVER_INFO(node, ...) ROS_INFO(__VA_ARGS__)
 #define DRIVER_WARN(node, ...) ROS_WARN(__VA_ARGS__)
 #define DRIVER_ERROR(node, ...) ROS_ERROR(__VA_ARGS__)
 #define DRIVER_FATAL(node, ...) ROS_FATAL(__VA_ARGS__)
 
+// 확장 디버그 매크로 정의
 #define DRIVER_DEBUG_EXTRA(node, EXTRA, ...) ROS_DEBUG_##EXTRA(__VA_ARGS__)
 #define DRIVER_INFO_EXTRA(node, EXTRA, ...) ROS_INFO_##EXTRA(__VA_ARGS__)
 #define DRIVER_WARN_EXTRA(node, EXTRA, ...) ROS_WARN_##EXTRA(__VA_ARGS__)
 #define DRIVER_ERROR_EXTRA(node, EXTRA, ...) ROS_ERROR_##EXTRA(__VA_ARGS__)
 #define DRIVER_FATAL_EXTRA(node, EXTRA, ...) ROS_FATAL_##EXTRA(__VA_ARGS__)
 
-#endif // ROS1_HEADERS_H_
+#endif // ROS1_HEADERS_H_ // ROS1_HEADERS_H_ 종료
