5ef8ff2d899f4d7000cb046562ff727ada31d912
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 10:57:09 +0900

    Update msg_MID360_launch.py

--- launch_ROS2/msg_MID360_launch.py
+++ launch_ROS2/msg_MID360_launch.py
@@ -2,22 +2,21 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import launch
 
-################### user configure parameters for ros2 start ###################
-xfer_format   = 1    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
-multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
-data_src      = 0    # 0-lidar, others-Invalid data src
-publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
-output_type   = 0
-frame_id      = 'livox_frame'
-lvx_file_path = '/home/livox/livox_test.lvx'
-cmdline_bd_code = 'livox0000000001'
+################### 사용자 설정 매개변수 시작 ###################
+xfer_format = 1    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
+multi_topic = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
+data_src = 0      # 0-lidar, others-Invalid data src
+publish_freq = 10.0 # 발행 주기, 5.0, 10.0, 20.0, 50.0 등
+output_type = 0    # 출력 데이터 형식
+frame_id = 'livox_frame' # 메시지 프레임 ID
+lvx_file_path = '/home/livox/livox_test.lvx' # lvx 파일 경로
+cmdline_bd_code = 'livox0000000001' # 기기 시리얼 번호
 
 cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
-cur_config_path = cur_path + '../config'
+cur_config_path = os.path.join(cur_path, '../config')
 user_config_path = os.path.join(cur_config_path, 'MID360_config.json')
-################### user configure parameters for ros2 end #####################
+################### 사용자 설정 매개변수 끝 #####################
 
 livox_ros2_params = [
     {"xfer_format": xfer_format},
@@ -31,7 +30,6 @@ livox_ros2_params = [
     {"cmdline_input_bd_code": cmdline_bd_code}
 ]
 
-
 def generate_launch_description():
     livox_driver = Node(
         package='livox_ros_driver2',
@@ -39,7 +37,7 @@ def generate_launch_description():
         name='livox_lidar_publisher',
         output='screen',
         parameters=livox_ros2_params
-        )
+    )
 
     return LaunchDescription([
         livox_driver,
