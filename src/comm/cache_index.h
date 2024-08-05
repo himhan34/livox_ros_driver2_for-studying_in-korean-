608b74df423ed83b497e580e6def08dbebe2834d
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 09:48:19 +0900

    Update cache_index.h

--- src/comm/cache_index.h
+++ src/comm/cache_index.h
@@ -22,33 +22,38 @@
 // SOFTWARE.
 //
 
-#ifndef LIVOX_ROS_DRIVER_CACHE_INDEX_H_
-#define LIVOX_ROS_DRIVER_CACHE_INDEX_H_
+#ifndef LIVOX_ROS_DRIVER_CACHE_INDEX_H_ // LIVOX_ROS_DRIVER_CACHE_INDEX_H_가 정의되지 않았을 때
+#define LIVOX_ROS_DRIVER_CACHE_INDEX_H_ // LIVOX_ROS_DRIVER_CACHE_INDEX_H_를 정의
 
-#include <mutex>
-#include <array>
-#include <map>
-#include <string>
+#include <mutex> // 뮤텍스 사용을 위한 mutex 라이브러리 포함
+#include <array> // 배열 사용을 위한 array 라이브러리 포함
+#include <map> // 맵 사용을 위한 map 라이브러리 포함
+#include <string> // 문자열 처리를 위한 string 라이브러리 포함
 
-#include "comm/comm.h"
+#include "comm/comm.h" // "comm/comm.h" 헤더 파일 포함
 
-namespace livox_ros {
+namespace livox_ros { // livox_ros 네임스페이스 시작
 
-class CacheIndex {
+class CacheIndex { // CacheIndex 클래스 정의
  public:
-  CacheIndex();
+  CacheIndex(); // 생성자
+  // 비어있는 인덱스를 가져오는 함수
   int8_t GetFreeIndex(const uint8_t livox_lidar_type, const uint32_t handle, uint8_t& index);
+  // 인덱스를 가져오는 함수
   int8_t GetIndex(const uint8_t livox_lidar_type, const uint32_t handle, uint8_t& index);
+  // 인덱스 키를 생성하는 함수
   int8_t GenerateIndexKey(const uint8_t livox_lidar_type, const uint32_t handle, std::string& key);
+  // Livox 인덱스를 가져오는 함수
   int8_t LvxGetIndex(const uint8_t livox_lidar_type, const uint32_t handle, uint8_t& index);
+  // 인덱스를 초기화하는 함수
   void ResetIndex(LidarDevice *lidar);
 
  private:
-  std::mutex index_mutex_;
-  std::map<std::string, uint8_t> map_index_; /* key:handle/slot, val:index */
-  std::array<bool, kMaxSourceLidar> index_cache_;
+  std::mutex index_mutex_; // 인덱스 뮤텍스
+  std::map<std::string, uint8_t> map_index_; /* key:handle/slot, val:index */ // 인덱스 맵
+  std::array<bool, kMaxSourceLidar> index_cache_; // 인덱스 캐시 배열
 };
 
-} // namespace livox_ros
+} // namespace livox_ros // livox_ros 네임스페이스 끝
 
-# endif // LIVOX_ROS_DRIVER_CACHE_INDEX_H_
+#endif // LIVOX_ROS_DRIVER_CACHE_INDEX_H_ // LIVOX_ROS_DRIVER_CACHE_INDEX_H_ 종료
