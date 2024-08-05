fa704239df84880fec4ac33e2055d11cedaad1a1
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 09:46:31 +0900

    Update livox_lidar_callback.h

--- src/call_back/livox_lidar_callback.h
+++ src/call_back/livox_lidar_callback.h
@@ -22,50 +22,59 @@
 // SOFTWARE.
 //
 
-#ifndef LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_
-#define LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_
+#ifndef LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_ // LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_가 정의되지 않았을 때
+#define LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_ // LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_를 정의
 
-#include "../lds.h"
-#include "../lds_lidar.h"
-#include "../comm/comm.h"
+#include "../lds.h" // 상위 디렉토리의 "lds.h" 헤더 파일 포함
+#include "../lds_lidar.h" // 상위 디렉토리의 "lds_lidar.h" 헤더 파일 포함
+#include "../comm/comm.h" // 상위 디렉토리의 "comm/comm.h" 헤더 파일 포함
 
-#include "livox_lidar_api.h"
-#include "livox_lidar_def.h"
+#include "livox_lidar_api.h" // "livox_lidar_api.h" 헤더 파일 포함
+#include "livox_lidar_def.h" // "livox_lidar_def.h" 헤더 파일 포함
 
-namespace livox_ros {
+namespace livox_ros { // livox_ros 네임스페이스 시작
 
-class LivoxLidarCallback {
+class LivoxLidarCallback { // LivoxLidarCallback 클래스 정의
  public:
+  // Lidar 정보 변경 콜백 함수 (정적 함수)
   static void LidarInfoChangeCallback(const uint32_t handle,
                                       const LivoxLidarInfo* info,
                                       void* client_data);
+  // 작업 모드 변경 콜백 함수 (정적 함수)
   static void WorkModeChangedCallback(livox_status status,
                                       uint32_t handle,
                                       LivoxLidarAsyncControlResponse *response,
                                       void *client_data);
+  // 데이터 타입 설정 콜백 함수 (정적 함수)
   static void SetDataTypeCallback(livox_status status, uint32_t handle,
                                   LivoxLidarAsyncControlResponse *response,
                                   void *client_data);
+  // 패턴 모드 설정 콜백 함수 (정적 함수)
   static void SetPatternModeCallback(livox_status status, uint32_t handle,
                                      LivoxLidarAsyncControlResponse *response,
                                      void *client_data);
+  // 블라인드 스팟 설정 콜백 함수 (정적 함수)
   static void SetBlindSpotCallback(livox_status status, uint32_t handle,
                                    LivoxLidarAsyncControlResponse *response,
                                    void *client_data);
+  // 듀얼 방출 설정 콜백 함수 (정적 함수)
   static void SetDualEmitCallback(livox_status status, uint32_t handle,
                                   LivoxLidarAsyncControlResponse *response,
                                   void *client_data);
+  // 태도 설정 콜백 함수 (정적 함수)
   static void SetAttitudeCallback(livox_status status, uint32_t handle,
                                   LivoxLidarAsyncControlResponse *response,
                                   void *client_data);
+  // Livox Lidar IMU 데이터 활성화 콜백 함수 (정적 함수)
   static void EnableLivoxLidarImuDataCallback(livox_status status, uint32_t handle,
                                   LivoxLidarAsyncControlResponse *response,
                                   void *client_data);
 
  private:
+  // Lidar 장치 가져오기 함수 (정적 함수)
   static LidarDevice* GetLidarDevice(const uint32_t handle, void* client_data);
 };
 
-} // namespace livox_ros
+} // namespace livox_ros // livox_ros 네임스페이스 끝
 
-#endif  // LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_
+#endif  // LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_ // LIVOX_ROS_DRIVER_LIVOX_LIDAR_CALLBACK_H_ 종료
