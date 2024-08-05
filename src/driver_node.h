733fbdafeebd5c483b6a3a2b02a7f240c753eea1
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 10:16:56 +0900

    Update driver_node.h

--- src/driver_node.h
+++ src/driver_node.h
@@ -22,57 +22,57 @@
 // SOFTWARE.
 //
 
-#ifndef LIVOX_DRIVER_NODE_H
-#define LIVOX_DRIVER_NODE_H
+#ifndef LIVOX_DRIVER_NODE_H // LIVOX_DRIVER_NODE_H가 정의되지 않았을 때
+#define LIVOX_DRIVER_NODE_H // LIVOX_DRIVER_NODE_H를 정의
 
-#include "include/ros_headers.h"
+#include "include/ros_headers.h" // "ros_headers.h" 헤더 파일 포함
 
-namespace livox_ros {
+namespace livox_ros { // livox_ros 네임스페이스 시작
 
-class Lddc;
+class Lddc; // Lddc 클래스 선언
 
-#ifdef BUILDING_ROS1
-class DriverNode final : public ros::NodeHandle {
+#ifdef BUILDING_ROS1 // ROS1을 빌드할 때
+class DriverNode final : public ros::NodeHandle { // DriverNode 클래스 정의, ros::NodeHandle 상속
  public:
-  DriverNode() = default;
-  DriverNode(const DriverNode &) = delete;
-  ~DriverNode();
-  DriverNode &operator=(const DriverNode &) = delete;
+  DriverNode() = default; // 기본 생성자
+  DriverNode(const DriverNode &) = delete; // 복사 생성자 삭제
+  ~DriverNode(); // 소멸자
+  DriverNode &operator=(const DriverNode &) = delete; // 복사 할당 연산자 삭제
 
-  DriverNode& GetNode() noexcept;
+  DriverNode& GetNode() noexcept; // 노드 객체 반환 함수
 
-  void PointCloudDataPollThread();
-  void ImuDataPollThread();
+  void PointCloudDataPollThread(); // 포인트 클라우드 데이터 폴링 스레드 함수
+  void ImuDataPollThread(); // IMU 데이터 폴링 스레드 함수
 
-  std::unique_ptr<Lddc> lddc_ptr_;
-  std::shared_ptr<std::thread> pointclouddata_poll_thread_;
-  std::shared_ptr<std::thread> imudata_poll_thread_;
-  std::shared_future<void> future_;
-  std::promise<void> exit_signal_;
+  std::unique_ptr<Lddc> lddc_ptr_; // Lddc 포인터
+  std::shared_ptr<std::thread> pointclouddata_poll_thread_; // 포인트 클라우드 데이터 폴링 스레드
+  std::shared_ptr<std::thread> imudata_poll_thread_; // IMU 데이터 폴링 스레드
+  std::shared_future<void> future_; // 종료 신호를 위한 미래 객체
+  std::promise<void> exit_signal_; // 종료 신호를 위한 약속 객체
 };
 
-#elif defined BUILDING_ROS2
-class DriverNode final : public rclcpp::Node {
+#elif defined BUILDING_ROS2 // ROS2를 빌드할 때
+class DriverNode final : public rclcpp::Node { // DriverNode 클래스 정의, rclcpp::Node 상속
  public:
-  explicit DriverNode(const rclcpp::NodeOptions& options);
-  DriverNode(const DriverNode &) = delete;
-  ~DriverNode();
-  DriverNode &operator=(const DriverNode &) = delete;
+  explicit DriverNode(const rclcpp::NodeOptions& options); // 생성자
+  DriverNode(const DriverNode &) = delete; // 복사 생성자 삭제
+  ~DriverNode(); // 소멸자
+  DriverNode &operator=(const DriverNode &) = delete; // 복사 할당 연산자 삭제
 
-  DriverNode& GetNode() noexcept;
+  DriverNode& GetNode() noexcept; // 노드 객체 반환 함수
 
  private:
-  void PointCloudDataPollThread();
-  void ImuDataPollThread();
+  void PointCloudDataPollThread(); // 포인트 클라우드 데이터 폴링 스레드 함수
+  void ImuDataPollThread(); // IMU 데이터 폴링 스레드 함수
 
-  std::unique_ptr<Lddc> lddc_ptr_;
-  std::shared_ptr<std::thread> pointclouddata_poll_thread_;
-  std::shared_ptr<std::thread> imudata_poll_thread_;
-  std::shared_future<void> future_;
-  std::promise<void> exit_signal_;
+  std::unique_ptr<Lddc> lddc_ptr_; // Lddc 포인터
+  std::shared_ptr<std::thread> pointclouddata_poll_thread_; // 포인트 클라우드 데이터 폴링 스레드
+  std::shared_ptr<std::thread> imudata_poll_thread_; // IMU 데이터 폴링 스레드
+  std::shared_future<void> future_; // 종료 신호를 위한 미래 객체
+  std::promise<void> exit_signal_; // 종료 신호를 위한 약속 객체
 };
 #endif
 
-} // namespace livox_ros
+} // namespace livox_ros // livox_ros 네임스페이스 끝
 
-#endif // LIVOX_DRIVER_NODE_H
+#endif // LIVOX_DRIVER_NODE_H // LIVOX_DRIVER_NODE_H 종료
