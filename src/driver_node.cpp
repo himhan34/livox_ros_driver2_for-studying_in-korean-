bd4b60406183249815c9c6fd0f9ef90630e244eb
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 10:15:59 +0900

    Update driver_node.cpp

--- src/driver_node.cpp
+++ src/driver_node.cpp
@@ -22,25 +22,22 @@
 // SOFTWARE.
 //
 
-#include "driver_node.h"
-#include "lddc.h"
+#include "driver_node.h" // "driver_node.h" 헤더 파일 포함
+#include "lddc.h" // "lddc.h" 헤더 파일 포함
 
-namespace livox_ros {
+namespace livox_ros { // livox_ros 네임스페이스 시작
 
+// DriverNode 클래스의 GetNode 함수 정의
 DriverNode& DriverNode::GetNode() noexcept {
-  return *this;
+  return *this; // 현재 객체를 반환
 }
 
+// DriverNode 클래스의 소멸자 정의
 DriverNode::~DriverNode() {
-  lddc_ptr_->lds_->RequestExit();
-  exit_signal_.set_value();
-  pointclouddata_poll_thread_->join();
-  imudata_poll_thread_->join();
+  lddc_ptr_->lds_->RequestExit(); // LDDC의 LDS에 종료 요청
+  exit_signal_.set_value(); // 종료 신호 설정
+  pointclouddata_poll_thread_->join(); // 포인트 클라우드 데이터 폴링 스레드 종료 대기
+  imudata_poll_thread_->join(); // IMU 데이터 폴링 스레드 종료 대기
 }
 
-} // namespace livox_ros
-
-
-
-
-
+} // namespace livox_ros // livox_ros 네임스페이스 끝
