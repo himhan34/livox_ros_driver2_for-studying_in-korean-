474103c847cd6b4085e9645335bf341276a4376e
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 09:52:23 +0900

    Update lidar_imu_data_queue.h

--- src/comm/lidar_imu_data_queue.h
+++ src/comm/lidar_imu_data_queue.h
@@ -22,56 +22,54 @@
 // SOFTWARE.
 //
 
-#ifndef LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_
-#define LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_
 
-#include <list>
-#include <mutex>
-#include <cstdint>
+#ifndef LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_ // LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_가 정의되지 않았을 때
+#define LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_ // LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_를 정의
 
-namespace livox_ros {
+#include <list> // 리스트 사용을 위한 헤더 파일 포함
+#include <mutex> // 뮤텍스 사용을 위한 헤더 파일 포함
+#include <cstdint> // 정수형 데이터 타입 사용을 위한 헤더 파일 포함
 
-// Based on the IMU Data Type in Livox communication protocol
-// TODO: add a link to the protocol
+namespace livox_ros { // livox_ros 네임스페이스 시작
+
+// Livox 통신 프로토콜의 IMU 데이터 타입 기반
+// TODO: 프로토콜에 대한 링크 추가
 typedef struct {
-  float gyro_x;        /**< Gyroscope X axis, Unit:rad/s */
-  float gyro_y;        /**< Gyroscope Y axis, Unit:rad/s */
-  float gyro_z;        /**< Gyroscope Z axis, Unit:rad/s */
-  float acc_x;         /**< Accelerometer X axis, Unit:g */
-  float acc_y;         /**< Accelerometer Y axis, Unit:g */
-  float acc_z;         /**< Accelerometer Z axis, Unit:g */
+  float gyro_x;        /**< 자이로스코프 X 축, 단위: rad/s */
+  float gyro_y;        /**< 자이로스코프 Y 축, 단위: rad/s */
+  float gyro_z;        /**< 자이로스코프 Z 축, 단위: rad/s */
+  float acc_x;         /**< 가속도계 X 축, 단위: g */
+  float acc_y;         /**< 가속도계 Y 축, 단위: g */
+  float acc_z;         /**< 가속도계 Z 축, 단위: g */
 } RawImuPoint;
 
+// IMU 데이터 구조체
 typedef struct {
-  uint8_t lidar_type;
-  uint32_t handle;
-  uint8_t slot;
-  // union {
-  //   uint8_t handle;
-  //   uint8_t slot;
-  // };
-  uint64_t time_stamp;
-  float gyro_x;        /**< Gyroscope X axis, Unit:rad/s */
-  float gyro_y;        /**< Gyroscope Y axis, Unit:rad/s */
-  float gyro_z;        /**< Gyroscope Z axis, Unit:rad/s */
-  float acc_x;         /**< Accelerometer X axis, Unit:g */
-  float acc_y;         /**< Accelerometer Y axis, Unit:g */
-  float acc_z;         /**< Accelerometer Z axis, Unit:g */
+  uint8_t lidar_type;  // Lidar 타입
+  uint32_t handle;     // 핸들
+  uint8_t slot;        // 슬롯
+  uint64_t time_stamp; // 타임스탬프
+  float gyro_x;        /**< 자이로스코프 X 축, 단위: rad/s */
+  float gyro_y;        /**< 자이로스코프 Y 축, 단위: rad/s */
+  float gyro_z;        /**< 자이로스코프 Z 축, 단위: rad/s */
+  float acc_x;         /**< 가속도계 X 축, 단위: g */
+  float acc_y;         /**< 가속도계 Y 축, 단위: g */
+  float acc_z;         /**< 가속도계 Z 축, 단위: g */
 } ImuData;
 
+// Lidar IMU 데이터 큐 클래스
 class LidarImuDataQueue {
  public:
-  void Push(ImuData* imu_data);
-  bool Pop(ImuData& imu_data);
-  bool Empty();
-  void Clear();
+  void Push(ImuData* imu_data); // IMU 데이터를 큐에 푸시하는 함수
+  bool Pop(ImuData& imu_data); // IMU 데이터를 큐에서 팝하는 함수
+  bool Empty(); // 큐가 비어있는지 확인하는 함수
+  void Clear(); // 큐를 비우는 함수
 
  private:
-  std::mutex mutex_;
-  std::list<ImuData> imu_data_queue_;
+  std::mutex mutex_; // 뮤텍스
+  std::list<ImuData> imu_data_queue_; // IMU 데이터 큐 리스트
 };
 
-} // namespace
+} // namespace livox_ros // livox_ros 네임스페이스 끝
 
-#endif // LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_
-
+#endif // LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_ // LIVOX_ROS_DRIVER_LIDAR_IMU_DATA_QUEUE_H_ 종료
