e93ffa1dc8692829f3aefa82afec4bf7f4533a68
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 10:01:08 +0900

    Update ros2_headers.h

--- src/include/ros2_headers.h
+++ src/include/ros2_headers.h
@@ -24,29 +24,31 @@
 
 // Denoting headers specifically used for building ROS1 Driver.
 
-#ifndef ROS2_HEADERS_H_
-#define ROS2_HEADERS_H_
+#ifndef ROS2_HEADERS_H_ // ROS2_HEADERS_H_가 정의되지 않았을 때
+#define ROS2_HEADERS_H_ // ROS2_HEADERS_H_를 정의
 
-#include <thread>
-#include <future>
+#include <thread> // 스레드 사용을 위한 헤더 파일 포함
+#include <future> // 비동기 작업을 위한 헤더 파일 포함
 
-#include <rclcpp/rclcpp.hpp>
-#include <pcl_conversions/pcl_conversions.h>
-#include <sensor_msgs/msg/point_cloud2.hpp>
-#include <sensor_msgs/msg/imu.hpp>
-#include "livox_ros_driver2/msg/custom_point.hpp"
-#include "livox_ros_driver2/msg/custom_msg.hpp"
+#include <rclcpp/rclcpp.hpp> // ROS2 기본 헤더 파일 포함
+#include <pcl_conversions/pcl_conversions.h> // PCL 변환을 위한 헤더 파일 포함
+#include <sensor_msgs/msg/point_cloud2.hpp> // 포인트 클라우드2 메시지 사용을 위한 헤더 파일 포함
+#include <sensor_msgs/msg/imu.hpp> // IMU 센서 메시지 사용을 위한 헤더 파일 포함
+#include "livox_ros_driver2/msg/custom_point.hpp" // CustomPoint 메시지 사용을 위한 헤더 파일 포함
+#include "livox_ros_driver2/msg/custom_msg.hpp" // CustomMsg 메시지 사용을 위한 헤더 파일 포함
 
+// 디버그 매크로 정의
 #define DRIVER_DEBUG(node, ...) RCLCPP_DEBUG((node).get_logger(), __VA_ARGS__)
 #define DRIVER_INFO(node, ...) RCLCPP_INFO((node).get_logger(), __VA_ARGS__)
 #define DRIVER_WARN(node, ...) RCLCPP_WARN((node).get_logger(), __VA_ARGS__)
 #define DRIVER_ERROR(node, ...) RCLCPP_ERROR((node).get_logger(), __VA_ARGS__)
 #define DRIVER_FATAL(node, ...) RCLCPP_FATAL((node).get_logger(), __VA_ARGS__)
 
+// 확장 디버그 매크로 정의
 #define DRIVER_DEBUG_EXTRA(node, EXTRA, ...) RCLCPP_DEBUG_##EXTRA((node).get_logger(), __VA_ARGS__)
 #define DRIVER_INFO_EXTRA(node, EXTRA, ...) RCLCPP_INFO_##EXTRA((node).get_logger(), __VA_ARGS__)
 #define DRIVER_WARN_EXTRA(node, EXTRA, ...) RCLCPP_WARN_##EXTRA((node).get_logger(), __VA_ARGS__)
 #define DRIVER_ERROR_EXTRA(node, EXTRA, ...) RCLCPP_ERROR_##EXTRA((node).get_logger(), __VA_ARGS__)
 #define DRIVER_FATAL_EXTRA(node, EXTRA, ...) RCLCPP_FATAL_##EXTRA((node).get_logger(), __VA_ARGS__)
 
-#endif // ROS2_HEADERS_H_
+#endif // ROS2_HEADERS_H_ // ROS2_HEADERS_H_ 종료
