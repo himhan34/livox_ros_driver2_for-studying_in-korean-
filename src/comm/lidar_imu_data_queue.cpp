d23d5941e9f5b3291b1e41369647c2af9150298b
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-08-05 09:51:33 +0900

    Update lidar_imu_data_queue.cpp

--- src/comm/lidar_imu_data_queue.cpp
+++ src/comm/lidar_imu_data_queue.cpp
@@ -21,50 +21,53 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
+#include "lidar_imu_data_queue.h" // "lidar_imu_data_queue.h" 헤더 파일 포함
 
-#include "lidar_imu_data_queue.h"
+namespace livox_ros { // livox_ros 네임스페이스 시작
 
-namespace livox_ros {
-
+// IMU 데이터를 큐에 푸시하는 함수
 void LidarImuDataQueue::Push(ImuData* imu_data) {
-  ImuData data;
-  data.lidar_type = imu_data->lidar_type;
-  data.handle = imu_data->handle;
-  data.time_stamp = imu_data->time_stamp;
+  ImuData data; // 임시 ImuData 객체 생성
+  data.lidar_type = imu_data->lidar_type; // lidar_type 설정
+  data.handle = imu_data->handle; // handle 설정
+  data.time_stamp = imu_data->time_stamp; // time_stamp 설정
 
-  data.gyro_x = imu_data->gyro_x;
-  data.gyro_y = imu_data->gyro_y;
-  data.gyro_z = imu_data->gyro_z;
+  data.gyro_x = imu_data->gyro_x; // gyro_x 설정
+  data.gyro_y = imu_data->gyro_y; // gyro_y 설정
+  data.gyro_z = imu_data->gyro_z; // gyro_z 설정
 
-  data.acc_x = imu_data->acc_x;
-  data.acc_y = imu_data->acc_y;
-  data.acc_z = imu_data->acc_z;
+  data.acc_x = imu_data->acc_x; // acc_x 설정
+  data.acc_y = imu_data->acc_y; // acc_y 설정
+  data.acc_z = imu_data->acc_z; // acc_z 설정
 
-  std::lock_guard<std::mutex> lock(mutex_);
-  imu_data_queue_.push_back(std::move(data));
+  std::lock_guard<std::mutex> lock(mutex_); // 뮤텍스 잠금
+  imu_data_queue_.push_back(std::move(data)); // 데이터 큐에 추가
 }
 
+// IMU 데이터를 큐에서 팝하는 함수
 bool LidarImuDataQueue::Pop(ImuData& imu_data) {
-  std::lock_guard<std::mutex> lock(mutex_);
-  if (imu_data_queue_.empty()) {
-    return false;
+  std::lock_guard<std::mutex> lock(mutex_); // 뮤텍스 잠금
+  if (imu_data_queue_.empty()) { // 큐가 비어있는지 확인
+    return false; // 실패
   }
-  imu_data = imu_data_queue_.front();
-  imu_data_queue_.pop_front();
-  return true;
+  imu_data = imu_data_queue_.front(); // 큐의 첫 번째 데이터를 imu_data에 복사
+  imu_data_queue_.pop_front(); // 첫 번째 데이터 제거
+  return true; // 성공
 }
 
+// 큐가 비어있는지 확인하는 함수
 bool LidarImuDataQueue::Empty() {
-  std::lock_guard<std::mutex> lock(mutex_);
-  return imu_data_queue_.empty();
+  std::lock_guard<std::mutex> lock(mutex_); // 뮤텍스 잠금
+  return imu_data_queue_.empty(); // 큐가 비어있는지 여부 반환
 }
 
+// 큐를 비우는 함수
 void LidarImuDataQueue::Clear() {
-  std::list<ImuData> tmp_imu_data_queue;
+  std::list<ImuData> tmp_imu_data_queue; // 임시 큐 생성
   {
-    std::lock_guard<std::mutex> lock(mutex_);
-    imu_data_queue_.swap(tmp_imu_data_queue);
+    std::lock_guard<std::mutex> lock(mutex_); // 뮤텍스 잠금
+    imu_data_queue_.swap(tmp_imu_data_queue); // 기존 큐와 임시 큐를 교체
   }
 }
 
-} // namespace livox_ros
+} // namespace livox_ros // livox_ros 네임스페이스 끝
